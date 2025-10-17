56b9261e7904e8c36fc46cf5cce71d6cce511fc3
Thomas Schar <a1799855@adelaide.edu.au>
2025-10-17 14:54:29 +1030

    Initial writeup of unit class

---
+++ src/Unit.h
@@ -0,0 +1,34 @@
+#ifndef UNIT_H
+#define UNIT_H
+
+#include <iostream>
+#include "Entity.h"
+// #include "Faction.h"
+
+using namespace std;
+
+class Unit : public Entity {
+    private:
+        int atk;                // Attack power
+        int range;              // Distance from which unit can attack enemy
+        float atk_cd;           // Attack cooldown?
+        float atkTimer;         // Time between when unit can attack?
+        int speed;              // Speed of unit across battlefield
+        int cost;               // Expense of the unit
+        // Faction* faction;    // Will require addition of 'faction'
+    public:
+        Unit(int id, int hp, float pos, int atk, int range, float atk_cd, float atkTimer, int speed, int cost);
+    
+        int getAttack() const;            // Get 'atk' value
+        int getRange() const;             // Get 'range' value
+        float getAttackCooldown() const;  // Get 'atk_cd' value
+        float getAttackTimer() const;     // Get 'atkTimer' value
+        int getSpeed() const;             // Get 'speed' value
+        int getCost() const;              // Get 'cost' value
+    
+        bool canAfford(int cost);
+        virtual void attack(Entity* target);
+        virtual void update(float dt);
+};
+
+#endif
