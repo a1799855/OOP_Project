320ae92a5e56d47a448434c4be8b7c72bc8a8fb4
Thomas Schar <a1799855@adelaide.edu.au>
2025-10-22 01:49:04 +1030

    Added symbols for ease in rendering

--- src/Entity.cpp
+++ src/Entity.cpp
@@ -1,7 +1,7 @@
 #include "Entity.h"
 
 Entity::Entity(int id, int hp, float pos)
-    : id(id), hp(hp), pos(pos), alive(true) {}
+    : id(id), hp(hp), pos(pos), alive(true), playerSymb(' '), enemySymb(' ') {}
 
 void Entity::update(float dt) {
   // default: do nothing
@@ -18,5 +18,8 @@ void Entity::takeDamage(int dmg) {
 int Entity::getHp() const { return hp; }
 int Entity::getID() const { return id; }
 float Entity::getPos() const { return pos; }
+char Entity::getPlayerSymb() const { return playerSymb; }
+char Entity::getEnemySymb() const { return enemySymb; }
 void Entity::setPos(float p){pos = p;}
+void Entity::setSymb(char a, char b){playerSymb = a; enemySymb = b;}
 bool Entity::isAlive() const { return alive; }
--- src/Entity.h
+++ src/Entity.h
@@ -3,24 +3,29 @@
 
 class Entity {
  protected:
-    int id;
-    int hp;
-    float pos;    // MUST be protected for Projectile::update() to run
-    bool alive;
+   int id;
+   int hp;
+   float pos;    // MUST be protected for Projectile::update() to run
+   bool alive;
+   char playerSymb;
+   char enemySymb;
 
  public:
-    Entity(int id, int hp, float pos); 
-    virtual ~Entity() = default;
+   Entity(int id, int hp, float pos); 
+   virtual ~Entity() = default;
 
-    virtual void update(float dt);     
-    virtual void takeDamage(int dmg);
-    //virtual void movement(float pos);  
+   virtual void update(float dt);     
+   virtual void takeDamage(int dmg);
+   //virtual void movement(float pos);  
 
-    int getHp() const;
-    int getID() const;
-    float getPos() const;
-    void setPos(float p);
-    bool isAlive() const;
+   int getHp() const;
+   int getID() const;
+   float getPos() const;
+   char getPlayerSymb() const;
+   char getEnemySymb() const;
+   void setPos(float p);
+   void setSymb(char a, char b);
+   bool isAlive() const;
 };
 
 #endif
