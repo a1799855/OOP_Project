e9e7d40758af8a18fef07795de1575ac2e97cc2e
Thomas Schar <a1799855@adelaide.edu.au>
2025-10-22 12:28:54 +1030

    Added logging function

--- src/Economy.cpp
+++ src/Economy.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include "Economy.h"
+#include "Debug.h"
 
 using namespace std;
 
@@ -19,3 +22,23 @@ void Economy::update(float dt){ gold = gold + incomeRate; }
 void Economy::setIncomeRate(float inc){incomeRate = inc;}   // Set rate of income
 int Economy::getGold() const {return gold;}                 // Get current amount of gold
 float Economy::getIncomeRate() const {return incomeRate;}   // Get current rate of income
+
+// Create log to output entire state of object
+void Economy::logging(){
+    // If file exists, open and delete content (trunc)
+    // If file doesn't exist, create it 
+    ofstream file("log_Economy.txt", ios::out | ios::trunc);
+
+    // Exit function & output to debugger if log file couldn't open
+    if (!file){
+        Debug::print("Could not open log file in Economy");
+        return;
+    }
+
+    file << "Gold count: " << gold << endl;
+    file << "Income rate: " << incomeRate << endl;
+    file << "Accumulation value: " << acc << endl;
+
+    file.close();
+    Debug::print("Successfully wrote to Economy log file");
+}
